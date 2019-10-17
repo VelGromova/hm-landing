2a670be8d8126131df57554466c181eb5f2d1ce1
valeria <vel.gromova@gmail.com>
2019-10-17 23:43:33 +0200

    Add router view

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="carousel">
+        <carousel />
+    </div>
+</template>
+
+<script>
+import Carousel from '../Carousel'
+
+export default {
+  name: 'HomeComponent',
+  components: {
+    Carousel
+  }
+}
+</script>
